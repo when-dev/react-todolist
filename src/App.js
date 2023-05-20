f4cd193f4b6c4781f92625d5f5ab319bab4eb72b
whendev <ururugeorge@gmail.com>
2023-05-20 14:45:27 +0300

    start of development todolist

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import TodoList from './TodoList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const date1 = new Date(2021, 7, 19, 14, 5);
+const date2 = new Date(2021, 7, 19, 15, 23);
+
+const initialData = [
+  {
+    title: 'Изучить React',
+    desc: 'Да поскорее!',
+    image: '',
+    done: true,
+    createdAt: date1.toLocaleString(),
+    key: date1.getTime()
+  },
+  {
+    title: 'Написать первое React-приложение',
+    desc: 'Список запланированных дел',
+    image: '',
+    done: false,
+    createdAt: date2.toLocaleString(),
+    key: date2.getTime()
+  }
+]
+
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.data = initialData;
+  }
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar is-light">
+          <div className="navbar-brand">
+            <span className="navbar-item is-uppercase">
+              TodoList
+            </span>
+          </div>
+        </nav>
+        <main className="content px-6 mt-6">
+          <TodoList list={this.data} />
+        </main>
+      </div>
+    )
+  }
 }
-
-export default App;
