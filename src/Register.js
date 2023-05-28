6de74feb796664b9bb9f8896dafab231e28c485c
whendev <ururugeorge@gmail.com>
2023-05-28 17:17:47 +0300

    Validation and error reporting

--- src/Register.js
+++ src/Register.js
@@ -5,8 +5,10 @@ import { register } from './auth';
 export default class Register extends Component {
   constructor(props) {
     super(props);
+    this.state = { errorEmail: '', errorPassword: '', errorPasswordConfirm: '' };
     this.handleEmailChange = this.handleEmailChange.bind(this);
     this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.handlePasswordConfirmChange = this.handlePasswordConfirmChange.bind(this);
     this.handleFormSubmit = this.handleFormSubmit.bind(this);
     this.clearFormData();
   }
@@ -14,7 +16,8 @@ export default class Register extends Component {
   clearFormData() {
     this.formData = {
       email: '',
-      password: ''
+      password: '',
+      passwordConfirm: ''
     };
   }
 
@@ -26,11 +29,69 @@ export default class Register extends Component {
     this.formData.password = evt.target.value;
   }
 
+  handlePasswordConfirmChange(evt) {
+    this.formData.passwordConfirm = evt.target.value;
+  }
+
   async handleFormSubmit(evt) {
     evt.preventDefault();
-    const result = await register(this.formData.email, this.formData.password);
-    if (typeof result != 'object')
-      console.log(result);
+    if (this.validate()) {
+      const result = await register(this.formData.email, this.formData.password);
+      if (typeof result != 'object')
+        this.showErrorMessage(result);
+    }
+  }
+
+  resetErrorMessages() {
+    this.setState((state) => ({
+      errorEmail: '',
+      errorPassword: '',
+      errorPasswordConfirm: ''
+    }));
+  }
+
+  validate() {
+    this.resetErrorMessages();
+    if (!this.formData.email) {
+      this.setState((state) => ({
+        errorEmail: 'Адрес электронной почты не указан'
+      }));
+      return false;
+    }
+    if (!this.formData.password) {
+      this.setState((state) => ({
+        errorEmail: 'Пароль не указан'
+      }));
+      return false;
+    }
+    if (!this.formData.passwordConfirm) {
+      this.setState((state) => ({
+        errorEmail: 'Повтор пароля не указан'
+      }));
+      return false;
+    }
+    if (this.formData.password !== this.formData.passwordConfirm) {
+      this.setState((state) => ({
+        errorPassword: 'Введеные пароли не совпадают',
+        errorPasswordConfirm: 'Введеные пароли не совпадают'
+      }))
+      return false;
+    }
+    return true;
+  }
+
+  showErrorMessage(code) {
+    this.resetErrorMessages();
+    if (code === 'auth/email-already-in-use') {
+      this.setState((state) => ({
+        errorEmail: 'Пользователь с таким адресом электронной ' + ' почты уже зарегистрирован'
+      }));
+    } else if (code === 'auth/weak-password') {
+      this.setState((state) => ({
+        errorPassword: 'Слишком простой пароль',
+        errorPasswordConfirm: 'Слишком простой пароль'
+      }));
+    }
   }
 
   render() {
@@ -46,12 +107,33 @@ export default class Register extends Component {
               <div className='control'>
                 <input type='email' className='input' onChange={this.handleEmailChange} />
               </div>
+              {this.state.errorEmail &&
+                <p className='help is-danger'>
+                  {this.state.errorEmail}
+                </p>
+              }
             </div>
             <div className='field'>
               <label className='label'>Пароль</label>
               <div className='control'>
                 <input type='password' className='input' onChange={this.handlePasswordChange} />
               </div>
+              {this.state.errorPassword &&
+                <p className='help is-danger'>
+                  {this.state.errorPassword}
+                </p>
+              }
+            </div>
+            <div className='field'>
+              <label className='label'>Повтор пароля</label>
+              <div className='control'>
+                <input type='password' className='input' onChange={this.handlePasswordConfirmChange} />
+              </div>
+              {this.state.errorPasswordConfirm &&
+                <p className='help is-danger'>
+                  {this.state.errorPasswordConfirm}
+                </p>
+              }
             </div>
             <div className='field is-grouped is-grouped-right'>
               <div className='control'>
